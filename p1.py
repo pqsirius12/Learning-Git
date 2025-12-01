960e027be9506f75d38e16b4118032c0b6a9e1e9
PQ <pqsirius12@gmail.com>
2025-12-01 16:24:24 +0530

    3nd commit on main branch

--- p1.py
+++ p1.py
@@ -1,3 +1,12 @@
 
 print("Hello, world")
 print("A new language a new day.")
+def factorial(n):
+	if n<0:
+		return -1
+	elif n==0 or n==1:
+		return 1
+	else:
+		return n*factorial(n-1)
+n = int(input("Enter a number:"))
+print(factorial(n))
